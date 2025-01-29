aba5ab196e564e4ca095197b40f07266ea32246d
Arivuselvam S <149804407+Arivuselvam95@users.noreply.github.com>
2025-01-29 11:40:23 +0530

    Time: 1 ms (15.29%) | Memory: 41.1 MB (8.05%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.java
+++ 2645-pass-the-pillow/pass-the-pillow.java
@@ -1,9 +1,21 @@
 class Solution {
     public int passThePillow(int n, int time) {
-        int quotient = time / (n -1);
-        int remainder = time % (n -1);
-        if(quotient%2==0)
-            return remainder+1;
-        return n - remainder;
+        // int quotient = time / (n -1);
+        // int remainder = time % (n -1);
+        // if(quotient%2==0)
+        //     return remainder+1;
+        // return n - remainder;
+        int arr[] = new int[n*2-2];
+        for(int i=1;i<=n;i++)
+            arr[i-1]=i;
+        for(int i=1;i<n-1;i++)
+            arr[n+i-1]=n-i;
+        int i=0;
+        while(time!=0){
+            i++;
+            i%=arr.length;
+            time--;
+        }
+        return arr[i];
     }
 }
