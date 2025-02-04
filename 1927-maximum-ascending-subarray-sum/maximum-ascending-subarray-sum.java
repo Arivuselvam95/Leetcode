1cd0d4a08112621bb92451d205dacc9df294be1e
Arivuselvam S <149804407+Arivuselvam95@users.noreply.github.com>
2025-02-04 09:09:59 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (66.56%) - LeetSync

--- 1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.java
+++ 1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.java
@@ -3,8 +3,9 @@ class Solution {
         int maxSubarraySum = Integer.MIN_VALUE;
         int sum = nums[0];
         for(int i=1;i<nums.length;i++){
-            if(nums[i-1]<nums[i])
+            if(nums[i-1]<nums[i]){
                 sum+=nums[i];
+            }
             else{
                 maxSubarraySum = maxSubarraySum < sum ? sum : maxSubarraySum;
                 sum=nums[i];
