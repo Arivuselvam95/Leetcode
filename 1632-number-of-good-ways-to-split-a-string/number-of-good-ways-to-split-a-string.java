021cdc1a6b0d8ff3c3df662441eafc84ece1d76d
Arivuselvam S <149804407+Arivuselvam95@users.noreply.github.com>
2025-02-14 14:28:07 +0530

    Time: 14 ms (69.76%) | Memory: 44.7 MB (71.22%) - LeetSync

--- 1632-number-of-good-ways-to-split-a-string/number-of-good-ways-to-split-a-string.java
+++ 1632-number-of-good-ways-to-split-a-string/number-of-good-ways-to-split-a-string.java
@@ -2,21 +2,18 @@ class Solution {
     public int numSplits(String s) {
         int n = s.length();
         int[] left = new int[n-1];
-        int[] right = new int[n-1];
         Set<Character> set = new HashSet<>();
         for(int i=0;i<n-1;i++){
             set.add(s.charAt(i));
             left[i] = set.size();
         }
         set = new HashSet<>();
+        int count = 0;
         for(int i=n-1;i>0;i--){
             set.add(s.charAt(i));
-            right[i-1] = set.size();
-        }
-        int count = 0;
-        for(int i=0;i<n-1;i++)
-            if(left[i]==right[i])
+            if(left[i-1] == set.size())
                 count++;
+        }
         return count;
     }
 }
