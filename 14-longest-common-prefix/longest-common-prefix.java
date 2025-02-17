92a6605172919427df9b28978529e75d5ee8a507
Arivuselvam S <149804407+Arivuselvam95@users.noreply.github.com>
2025-02-17 13:52:22 +0530

    Time: 2 ms (22.44%) | Memory: 44.6 MB (5.02%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String ans="";
+        Arrays.sort(strs);
+        for(int i=0;i<strs[0].length();i++){
+            for(String str: strs){
+                if(!str.startsWith(strs[0].substring(0,i+1))){
+                    return ans;
+                }
+            }
+            ans = strs[0].substring(0,i+1);
+        }
+        return ans;
+    }
+}
