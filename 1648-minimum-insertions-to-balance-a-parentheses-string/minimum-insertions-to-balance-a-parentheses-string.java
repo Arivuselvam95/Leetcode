f8193eed4bc6a0624da3364ce74e57044bda7a08
Arivuselvam S <149804407+Arivuselvam95@users.noreply.github.com>
2025-02-26 10:03:47 +0530

    Time: 27 ms (41.64%) | Memory: 45.8 MB (26.52%) - LeetSync

---
+++ 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minInsertions(String s) {
+        Stack<Character> stack = new Stack<>();
+        int count = 0;
+        int i = 0;
+        while (i < s.length()) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(c);
+            } else { 
+                if (i + 1 < s.length() && s.charAt(i + 1) == ')') {
+                    i++;
+                } else {
+                    count++;
+                }
+                if (!stack.isEmpty()) {
+                    stack.pop(); 
+                } else {
+                    count++;
+                }
+            }
+            i++;
+        }
+        count += stack.size() * 2;
+        return count;
+    }
+}
