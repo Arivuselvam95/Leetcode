599bfb7a048d1a3a6f0db0790693f895a2f809e7
Arivuselvam S <149804407+Arivuselvam95@users.noreply.github.com>
2025-04-08 14:11:39 +0530

    Time: 1 ms (100.00%) | Memory: 44.8 MB (29.84%) - LeetSync

--- 674-longest-continuous-increasing-subsequence/longest-continuous-increasing-subsequence.java
+++ 674-longest-continuous-increasing-subsequence/longest-continuous-increasing-subsequence.java
@@ -3,19 +3,16 @@ class Solution {
         int n = nums.length;
         int ans = 0;
         
-        for(int i=0;i<n;i++){
-            int count=1;
-            for(int j=i;j<n-1;j++){
-                if(nums[j]<nums[j+1])
-                    count++;
-                else{
-                    ans = Math.max(ans,count);
-                    count=1;
-                    break;
-                }
+        int count=1;
+        for(int i=0;i<n-1;i++){
+            if(nums[i]<nums[i+1])
+                count++;
+            else{
+                ans = Math.max(ans,count);
+                count=1;
             }
-            ans = Math.max(ans,count);
         }
+        ans = Math.max(ans,count);
         return ans;
     }
 }
